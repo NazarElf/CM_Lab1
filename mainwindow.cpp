2d6655bd33ed6ffb66eb70f13487985e392ee93d
NazarElf <nnn43@ukr.net>
2017-10-15 18:16:02 +0300

    Organizing output

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -45,20 +45,38 @@ void MainWindow::on_Start_clicked()
     ProcessingUnit *PU1 = new FIFO(mu,lambda);
     ProcessingUnit *PU2 = new SF(mu,lambda);
     ProcessingUnit *PU3 = new RR(mu,lambda);
-    double b;
     PU3->tau = 0.0001;
     PU1->Start(inVec,poisson);
     PU2->Start(inVec,poisson);
     PU3->Start(inVec,poisson);
 
+    //counting functions
+    double FIFO_mx = PU1->getMx();
+    double SF_mx = PU2->getMx();
+    double RR_mx = PU3->getMx();
 
-    b = PU1->getMx();
-    double b2 = PU2->getMx();
-    double b3 = PU3->getMx();
+    double FIFO_Dx = PU1->getDx();
+    double SF_Dx = PU2->getDx();
+    double RR_Dx = PU3->getDx();
 
-    //double dd1 = PU1->getDx();
+    double FIFO_f = -5.0 * FIFO_mx - 2.0 * FIFO_Dx;
+    double SF_f = -5.0 * SF_mx - 2.0 * SF_Dx;
+    double RR_f = -5.0 * RR_mx - 2.0 * RR_Dx;
 
-    ui->Results->showMessage(QString::number(b), 0);
+    //organizing output
+    ui->FIFO_Dx->setText(QString::number(FIFO_Dx));
+    ui->FIFO_mx->setText(QString::number(FIFO_mx));
+    ui->FIFO_func->setText(QString::number(FIFO_f));
+
+    ui->SF_Dx->setText(QString::number(SF_Dx));
+    ui->SF_mx->setText(QString::number(SF_mx));
+    ui->SF_func->setText(QString::number(SF_f));
+
+    ui->RR_Dx->setText(QString::number(RR_Dx));
+    ui->RR_mx->setText(QString::number(RR_mx));
+    ui->RR_func->setText(QString::number(RR_f));
+
+    //ui->Results->showMessage(QString::number(b), 0);
     //delete PU;
 }
 
@@ -94,12 +112,42 @@ void MainWindow::on_Unusual_clicked()
 
     FIFO *PU = new FIFO(mu,lambda);
     PU->Start(inVec,poisson);
-/*
-    for(auto val: inVec)
-    {
-        std::cout << val << "\t";
-    }
-*/
+
+    //starting processor
+    ProcessingUnit *PU1 = new FIFO(mu,lambda);
+    ProcessingUnit *PU2 = new SF(mu,lambda);
+    ProcessingUnit *PU3 = new RR(mu,lambda);
+    PU3->tau = 0.0001;
+    PU1->Start(inVec,poisson);
+    PU2->Start(inVec,poisson);
+    PU3->Start(inVec,poisson);
+
+    //counting functions
+    double FIFO_mx = PU1->getMx();
+    double SF_mx = PU2->getMx();
+    double RR_mx = PU3->getMx();
+
+    double FIFO_Dx = PU1->getDx();
+    double SF_Dx = PU2->getDx();
+    double RR_Dx = PU3->getDx();
+
+    double FIFO_f = -5.0 * FIFO_mx - 2.0 * FIFO_Dx;
+    double SF_f = -5.0 * SF_mx - 2.0 * SF_Dx;
+    double RR_f = -5.0 * RR_mx - 2.0 * RR_Dx;
+
+    //organizing output
+    ui->FIFO_Dx->setText(QString::number(FIFO_Dx));
+    ui->FIFO_mx->setText(QString::number(FIFO_mx));
+    ui->FIFO_func->setText(QString::number(FIFO_f));
+
+    ui->SF_Dx->setText(QString::number(SF_Dx));
+    ui->SF_mx->setText(QString::number(SF_mx));
+    ui->SF_func->setText(QString::number(SF_f));
+
+    ui->RR_Dx->setText(QString::number(RR_Dx));
+    ui->RR_mx->setText(QString::number(RR_mx));
+    ui->RR_func->setText(QString::number(RR_f));
+
 }
 
 void MainWindow::genPoisson(int count, double lambda)
